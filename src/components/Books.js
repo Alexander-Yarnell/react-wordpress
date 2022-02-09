7cffdb4015e315d0f6bc6d9de910024b17aef23b
Alexander-Yarnell <yarnell.alexander@yahoo.com>
2022-02-09 20:30:43 +0000

    Test React-Wordpress on linode

---
+++ src/components/Books.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import axios from "axios";
+import BookItem from "./BookItem";
+
+export class Books extends Component {
+  state = {
+    books: [],
+    isLoaded: false,
+  };
+
+  componentDidMount() {
+    axios
+      .get("https://reactwp.bccdev00.com/wp-json/wp/v2/books")
+      .then((res) =>
+        this.setState({
+          books: res.data,
+          isLoaded: true,
+        })
+      )
+      .catch((err) => console.log(err));
+  }
+
+  render() {
+    const { books, isLoaded } = this.state;
+
+    if (isLoaded) {
+      return (
+        <div>
+          {books.map((book) => {
+            return (
+              <div>
+                <BookItem key={book.id} book={book}/>
+              </div>
+            );
+          })}
+        </div>
+      );
+    }
+    return <h3>Loading...</h3>;
+  }
+}
+
+export default Books;
