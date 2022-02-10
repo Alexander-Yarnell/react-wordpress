115028b9ae7ae6ae377ddda91dc1683ea4fe201f
Alexander-Yarnell <yarnell.alexander@yahoo.com>
2022-02-10 16:55:10 +0000

    removed Nav.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import Books from "./components/Books";
 
-import Nav from "./components/Nav";
 function App() {
   return (
     <div>
-      <Nav />
       <h1>App</h1>
       <Books />
     </div>
